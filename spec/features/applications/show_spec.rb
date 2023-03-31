c7b1720406dd4811a41a8f47a5a518bebfbc99d1
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-31 18:28:21 -0400

    test: add test for search by pet name for applications not yet submitted

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -3,18 +3,21 @@ require 'rails_helper'
 RSpec.describe 'the application show', type: :features do
   let!(:application_1) { Application.create!(name: 'Chris Simmons', street_address: '123 Main St.', city: 'Columbus', state: 'OH', zipcode: '43210', description: "I'm a good host!", status: 'Pending' )}
   let!(:application_2) { Application.create!(name: 'Jamison Ordway', street_address: '456 Main St.', city: 'Denver', state: 'CO', zipcode: '80202', description: "I'm a great host!", status: 'In Progress' )}
+  let!(:application_3) { Application.create!(name: 'Jane Doe', street_address: '1 South Street', city: 'Manhattan', state: 'NY', zipcode: '11231', description: "I love cats.", status: 'Approved' )}
+  let!(:application_4) { Application.create!(name: 'Mister Rogers', street_address: '9 North Street', city: 'Philadelphia', state: 'PA', zipcode: '19148', description: "I am so friendly.", status: 'Rejected' )}
+
 
   let!(:shelter_1) { Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)}
 
   let!(:pet_1) { shelter_1.pets.create!(name: 'Jasper', age: 7, breed: 'Maine Coon', adoptable: true )}
   let!(:pet_2) { shelter_1.pets.create!(name: 'Spot', age: 3, breed: 'Singapura', adoptable: true )}
+  let!(:pet_3) { shelter_1.pets.create!(name: 'Willow', age: 4, breed: 'Cornish Rex', adoptable: false )}
 
   it "shows the application and all it's attributes" do
     application_1.pets << pet_1
-    pet_2.applications << application_2
+    application_2.pets << pet_2
 
     visit "/applications/#{application_1.id}"
-    save_and_open_page
 
     expect(page).to have_content(application_1.name)
     expect(page).to have_content(application_1.street_address)
@@ -37,5 +40,51 @@ RSpec.describe 'the application show', type: :features do
     click_on(pet_1.name)
 
     expect(current_path).to eq("/pets/#{pet_1.id}")
+  end
+
+  it "lets non-submitted applications search for adoptable pets to add to the application" do
+    visit "/applications/#{application_2.id}"  #in progress application (not submitted)
+
+    expect(page).to have_content("Add a Pet to this Application")
+    expect(page).to have_content("Search for Pets by Name:")
+    expect(page).to have_field(:search)
+    expect(page).to have_button("Submit")
+
+    fill_in :search, with: "#{pet_1.name}"
+    click_button "Submit"
+    save_and_open_page
+
+    expect(current_path).to eq("/applications/#{application_2.id}")
+    expect(page).to have_content("Results")
+    expect(page).to have_content("#{pet_1.name}")
+    expect("Search for Pets by Name:").to appear_before("#{pet_1.name}")
+
+    expect(page).to_not have_content("#{pet_2.name}")
+    expect(page).to_not have_content("#{pet_3.name}")
+  end
+
+  it "shows a message if no pets match the search" do
+    visit "/applications/#{application_2.id}"
+
+    fill_in :search, with: "InvalidInput"
+    click_button "Submit"
+
+    expect(current_path).to eq("/applications/#{application_2.id}")
+    expect(page).to_not have_content("Results")
+    expect(page).to have_content("No pets match this search.")
+  end
+
+  it "does not display the search for applications that have already been submitted" do
+    visit "/applications/#{application_1.id}" #pending application (already submitted)
+    expect(page).to_not have_content("Add a Pet to this Application")
+    expect(page).to_not have_field(:search)
+
+    visit "/applications/#{application_3.id}" #approved application (already submitted)
+    expect(page).to_not have_content("Add a Pet to this Application")
+    expect(page).to_not have_field(:search)
+
+    visit "/applications/#{application_4.id}" #rejected application (already submitted)
+    expect(page).to_not have_content("Add a Pet to this Application")
+    expect(page).to_not have_field(:search)
   end
 end
