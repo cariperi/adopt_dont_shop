afeffee193d38f3bfd8098dca419c996e5dbf88c
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-31 18:29:15 -0400

    feat: add search by name feature for adoptable pets on application show page

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,5 +2,9 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+
+    if params[:search]
+      @found_pets = Pet.search(params[:search]).adoptable
+    end
   end
 end
