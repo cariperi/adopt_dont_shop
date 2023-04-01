de8fd8a7d5c3af5e8b293651a8dade095a5a5aa8
Julian Beldotti <julian.beldotti@gmail.com>
2023-03-31 21:40:25 -0500

    Feat: Add error messages if permitted params are invalid

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,10 @@
 class Application < ApplicationRecord
   has_many :pet_applications
   has_many :pets, through: :pet_applications
+
+  validates :name, presence: true
+  validates :street_address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 end
